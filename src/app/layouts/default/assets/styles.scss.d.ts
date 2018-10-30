8ba0387519d4b1c54925d45099286b2768d2315c
Eser Ozvataf <eser@ozvataf.com>
2018-10-30 07:28:31 +0300

    [WIP] Component scopes.

---
+++ src/app/layouts/default/assets/styles.scss.d.ts
@@ -0,0 +1,6 @@
+export const heroHead: string;
+export const navbar: string;
+export const heroBody: string;
+export const hero: string;
+export const isFullheight: string;
+export const heroFoot: string;
